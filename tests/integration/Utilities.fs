b4e0da5cc3393f7e654d44577817d49b08d14baa
b0wter <b0wter@gmx.de>
2019-09-26 19:52:15 +0200

    Added a base class for easier testing of a prefilled db.

--- tests/integration/Utilities.fs
+++ tests/integration/Utilities.fs
@@ -2,6 +2,9 @@ namespace b0wter.CouchDb.Tests.Integration
 
 module Utilities =
     
+    open Microsoft.FSharp.Quotations.Patterns
+    open Microsoft.FSharp.Reflection
+    
     [<AbstractClass>]
     type CleanDatabaseTests() =
         member this.IsAuthenticated = Initialization.authenticateCouchDbClient() |> Async.RunSynchronously 
@@ -9,3 +12,29 @@ module Utilities =
         
         member this.FailIfNotInitialized () =
             if this.IsAuthenticated && this.IsInitialized then () else failwith "The initialization has failed."
+            
+    
+    [<AbstractClass>]
+    type PrefilledDatabaseTests() =
+        inherit CleanDatabaseTests ()
+        
+        member this.RunWithDatabases dbNames (toRun: unit -> Async<unit>) =
+            async {
+                match! Initialization.createDatabases dbNames with
+                | true -> return! toRun ()
+                | false -> return failwith "The database preparation failed!"
+            } |> Async.RunSynchronously
+            
+        member this.Run (toRun: unit -> Async<unit>) =
+            async {
+                return! toRun ()
+            }
+
+    let unionCases<'a> () =
+        if FSharpType.IsUnion(typeof<'a>) then
+            let cases = FSharpType.GetUnionCases typeof<'a>
+            
+            FsUnit.CustomMatchers.True
+            0
+        else
+            1
