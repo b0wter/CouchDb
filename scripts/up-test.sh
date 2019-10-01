0c3d63f08680ad7073bd8a7ab9a39c98f6ca88b5
b0wter <b0wter@gmx.de>
2019-10-01 10:14:17 +0200

    Added the option to run a command.
    The up-test script can now run a command once couch db is available.

--- scripts/up-test.sh
+++ scripts/up-test.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+if [ "$#" -eq 0 ]; then
+	echo "Usage: up-test.sh <host> <port> [command]"
+	echo ""
+	echo "All arguments after <port> will be used to run as a new comand."
+	echo "This is useful if you want to send a message or beep after the database is available."
+	exit
+fi
+
 [ -z "$1" ] && echo "You need to supply the hostname as first parameter." && exit 1
 [ -z "$2" ] && echo "You need to supply the port as second parameter." && exit 1
 
 HOST="$1"
 PORT="$2"
+COMMAND="${@:3}"
 
 STATUSCODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://$HOST:$PORT/_up)
 
@@ -16,3 +25,10 @@ do
 done
 
 echo "CouchDb is now available!"
+
+if [ -z "$COMMAND" ]; then
+	# nothing to do
+	NOTHING="NOTHING"
+else
+	$COMMAND
+fi
