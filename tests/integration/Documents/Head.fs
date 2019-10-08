921017dca9952534d522c4b683de235d515ed8c9
b0wter <b0wter@gmx.de>
2019-10-08 21:29:04 +0200

    Removed duplicate dbName in Documents.Head

--- tests/integration/Documents/Head.fs
+++ tests/integration/Documents/Head.fs
@@ -11,18 +11,17 @@ module Head =
     
     type Tests() =
         inherit Utilities.EmptySingleDatabaseTests("test-db")
-        let dbName = "test-db"
         
         [<Fact>]
         member this.``Retrieving a newly-added document returns DocumentExists result`` () =
             async {
-                match! Databases.AddDocument.query Initialization.defaultDbProperties dbName Default.defaultInstance with
+                match! Databases.AddDocument.query Initialization.defaultDbProperties this.DbName Default.defaultInstance with
                 | Databases.AddDocument.Result.Created x ->
                     do x.ok |> should be True
                     do x.id |> should equal (Default.defaultInstance._id.ToString())
                     do x.rev |> should not' (be EmptyString)
                     
-                    match! Documents.Head.query Initialization.defaultDbProperties dbName Default.defaultInstance._id with
+                    match! Documents.Head.query Initialization.defaultDbProperties this.DbName Default.defaultInstance._id with
                     | Documents.Head.Result.DocumentExists y ->
                         y.ETag |> should equal x.rev
                     | x -> failwith <| sprintf "Expected NotModified but got %s" (x.GetType().FullName)
@@ -35,14 +34,14 @@ module Head =
         member this.``Retrieving a non-existing document returns NotFound`` () =
             async {
                 let id = System.Guid.Parse("3f4ae7a0-f4f3-489b-a3b8-eba22450fae4")
-                let! result = Documents.Head.query Initialization.defaultDbProperties dbName id
+                let! result = Documents.Head.query Initialization.defaultDbProperties this.DbName id
                 result |> should be (ofCase<@ Documents.Head.Result.NotFound @>)
             }
                 
         [<Fact>]
         member this.``Retrieving a document without specifying an id returns DocumentIdMissing`` () =
             async {
-                let! result = Documents.Head.query Initialization.defaultDbProperties dbName null
+                let! result = Documents.Head.query Initialization.defaultDbProperties this.DbName null
                 result |> should be (ofCase<@ Documents.Head.Result.DocumentIdMissing @>)
             }
                 
